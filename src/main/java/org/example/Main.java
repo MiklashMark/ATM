fd8d9a045796fbd5f77ecea324f2517d2894c6e7
Burukku1 <dima.dmitry06@gmail.com>
2023-08-10 14:47:27 +0300

    add sout in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,9 @@ import org.example.core.Core;
 import java.io.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("fgfsdhf");
+        System.out.println("fsd");
         Core core = new Core();
         try {
             core.start();
